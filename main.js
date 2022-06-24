f79bf195c8a78589125c115a41977a6e4e42316f
SrAnonimo13 <68660240+SrAnonimo13@users.noreply.github.com>
2022-06-24 12:41:22 -0300

    adicionando dificuldades

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ let last = {
 };
 
 let delay = false;
+let difficulty = 1; //0 = easy 1 = normal 2 = hard
+let difficultyHasChange = false;
 
 const colors = [
   "#00b8a9",
@@ -18,26 +20,64 @@ const colors = [
 ]
 
 const cards = [
-  { result: 9, texts: ['6 + 3', '3²', '3 * 3', '3²'] },
-  { result: 5, texts: ['√25', '2 + 3'] },
-  { result: 14, texts: ['7 * 2', '9 + 5'] },
-  { result: 25, texts: ['5 * 5', '30 - 5'] },
-  { result: 11, texts: ['44 - 33', '5 + 6'] },
-  { result: 4, texts: ['8 ÷ 2', '2 * 2'] },
-  { result: 1, texts: ['√1', '1 x 1'] },
-  { result: 16, texts: ['4²', '8 + 8'] },
-  { result: 30, texts: ['5 * 6', '15 + 15'] },
+  {
+    difficulty: 0,
+    cards: [
+      { result: 2, texts: ['1 + 1', '4 / 2'] },
+      { result: 6, texts: ['3 + 3', '4 + 2'] },
+      { result: 5, texts: ['10 / 2', '2 + 3'] },
+      { result: 8, texts: ['6 + 2', '11 - 3'] },
+      { result: 10, texts: ['5 + 5', '5 * 2'] },
+      { result: 10, texts: ['20 - 10', '30 - 20'] },
+      { result: 11, texts: ['5 + 6', '22 - 11'] },
+      { result: 1, texts: ['2 - 1', '1 * 1'] },
+      { result: 20, texts: ['10 * 2', '10 + 10'] },
+      { result: 30, texts: ['50 - 20', '20 + 10'] },
+    ]
+  },
+  {
+    difficulty: 1,
+    cards: [
+      { result: 9, texts: ['6 + 3', '3²', '3 * 3', '3²'] },
+      { result: 5, texts: ['√25', '2 + 3'] },
+      { result: 14, texts: ['7 * 2', '9 + 5'] },
+      { result: 25, texts: ['5 * 5', '30 - 5'] },
+      { result: 11, texts: ['44 - 33', '5 + 6'] },
+      { result: 4, texts: ['8 ÷ 2', '2 * 2'] },
+      { result: 1, texts: ['√1', '1 * 1'] },
+      { result: 16, texts: ['4²', '8 + 8'] },
+      { result: 30, texts: ['5 * 6', '15 + 15'] },
+    ]
+  },
+  {
+    difficulty: 2,
+    cards: [
+      { result: 5, texts: ['x2 = 10', '√25'] },
+      { result: 64, texts: ['8²', 'x4² = 4'] },
+      { result: 100, texts: ['10²', '√10000'] },
+      { result: 14, texts: ['7 * 2', '9 + 5'] },
+      { result: 25, texts: ['5 * 5', '30 - 5'] },
+      { result: 11, texts: ['44 - 33', '5 + 6'] },
+      { result: 4, texts: ['8 ÷ 2', '2 * 2'] },
+      { result: 1, texts: ['√1', '1 x 1'] },
+      { result: 16, texts: ['4²', '8 + 8'] },
+      { result: 30, texts: ['5 * 6', '15 + 15'] },
+    ]
+  }
 ]
 
 game.style.gridTemplateColumns = new Array(GameWidth).fill('1fr').join(' ');
 // POR FAVOR ANÔNIMO, NÃO QUEBRE O CÓDIGO
 // Ass: Moises
 const errorCounterDiv = document.getElementById('error');
-const winnerDiv = document.querySelector('.winner');
+const alertsDiv = document.querySelector('.alerts');
 const errosTotalDiv = document.getElementById('errorsTotal');
+const difficultyRange = document.getElementById('difficulty-range');
+const difficultyLabel = document.getElementById('difficulty');
 
 function init() {
-  for (const card of cards) {
+  console.log(cards, difficulty)
+  for (const card of cards.find(e => e.difficulty == difficulty).cards) {
     const color = colors[Math.floor(Math.random() * colors.length)];
 
     for (const text of card.texts) {
@@ -64,7 +104,7 @@ function init() {
           if (document.querySelectorAll('.correct').length === allCardsLength) {
             setTimeout(() => {
               errosTotalDiv.innerText = errorCounterDiv.innerText
-              winnerDiv.classList.add('show');
+              alertsDiv.classList.add('show', 'winner');
             }, 1000);
             return;
           }
@@ -108,6 +148,25 @@ function desviraVirados() {
 
 init();
 
+function showDifficult() {
+  const labels = {
+    0: 'Fácil',
+    1: 'Normal',
+    2: 'Difícil',
+  }
+
+  difficulty = difficultyRange.value;
+  difficultyLabel.innerText = labels[difficulty];
+}
+
+difficultyRange.value = difficulty;
+showDifficult();
+
+difficultyRange.addEventListener('input', () => {
+  difficultyHasChange = true;
+  showDifficult()
+});
+
 function ResetGame() {
   game.innerHTML = "";
   last = { div: null, result: null };
@@ -115,9 +174,25 @@ function ResetGame() {
   init();
 }
 
-document.getElementById('reset').addEventListener('click', () => ResetGame())
+document.getElementById('menu-button').addEventListener('click', () => {
+  alertsDiv.classList.add('show', 'menu');
+});
 
-document.getElementById('playagin').addEventListener('click', () => {
-  winnerDiv.classList.remove('show');
+document.getElementById('restart-winner').addEventListener('click', () => {
+  alertsDiv.classList.remove('show', 'winner');
   ResetGame();
 });
+
+document.getElementById('restart-menu').addEventListener('click', () => {
+  alertsDiv.classList.remove('show', 'menu');
+  ResetGame();
+});
+
+document.getElementById('playagin-menu').addEventListener('click', () => {
+  if (difficultyHasChange) {
+    ResetGame();
+  }
+
+  difficultyHasChange = false;
+  alertsDiv.classList.remove('show', 'menu');
+})
